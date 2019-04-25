05956e5ab715994e23ec9a28014daef7bf563738
Oriel Nofekh <42373011+Orielno@users.noreply.github.com>
2019-04-25 12:33:10 +0300

    Update files

--- Data Files Formatting/HPZ_Data_Format.m
+++ Data Files Formatting/HPZ_Data_Format.m
@@ -7,24 +7,25 @@ function [mat, success] = HPZ_Data_Format (file_path, locations)
 %   1 - column number of Subject ID in the matrix
 % 	2 - column number of observation number of the subject
 % 	3 - column number of the quantity of good 1 chosen by the subject.
-% 	4 - column number of the quantity of good 2 chosen by the subject.
+% 	4 - column number of the quantity of good k chosen by the subject.
+%       the columns of quantities of goods 1 to k are in locations(3):locations(4).   
 % 	5 - column number of the max quantity of good 1 (=1/price).
-% 	6 - column number of the max quantity of good 2 (=1/price).
+% 	6 - column number of the max quantity of good k (=1/price).
+%       the columns of max quantities of goods 1 to k are in locations(5):locations(6).   
 
 % It returns a matrix of data corresponding to the required treatment.
 % The matrix has six columns:
 % The first column is the subject ID.
 % The second column is the observation number - 50 observations per subject
-% The third column is the quantity of good 1 chosen by the subject.
-% The fourth column is the quantity of good 2 chosen by the subject.
-% The fifth column is the price of good 1. 
-% The sixth column is the price of good 2. 
+% Columns 3:(2+k) are the quantities of goods chosen by the subject.
+% Columns (3+k):(2+2k) are the price of goods. 
 
 try
 	% reading the data file
     [data] = csvread(file_path);
     % formatting the matrix as desired
-    mat = [data(:,locations(1)) data(:,locations(2)) data(:,locations(3)) data(:,locations(4)) 1./data(:,locations(5)) 1./data(:,locations(6))];
+    %mat = [data(:,locations(1)) data(:,locations(2)) data(:,locations(3)) data(:,locations(4)) 1./data(:,locations(5)) 1./data(:,locations(6))];
+    mat = [data(:, locations(1:2)), data(:, locations(3):locations(4)), 1./data(:, locations(5):locations(6))];
     success = true;
 catch
     warning(char(strcat(HPZ_Constants.could_not_read_file_1, {' '}, file_path, HPZ_Constants.could_not_read_file_2)));
