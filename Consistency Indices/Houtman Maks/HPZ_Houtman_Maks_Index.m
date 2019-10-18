94c7771c47c84be8e24bbf3fe97d292921a982b7
Oriel Nofekh <42373011+Orielno@users.noreply.github.com>
2019-10-18 11:06:44 +0300

    Update Houtman-Maks calculation functions
    Now we use intlinprog instead of my algorithm. The algorithm for finding minimal cycles is intact though.

---
+++ Consistency Indices/Houtman Maks/HPZ_Houtman_Maks_Index.m
@@ -0,0 +1,134 @@
+function [HM_mean, HM_sum, HM_raw_residuals] = HPZ_Houtman_Maks_Index (DRP, SDRP, identical_choice, out_of_sample_required)
+
+% for this specific algorithm (finding minimal cycles),
+% we want DRP to not contain 1 in (i,j) if i and j are identical choices 
+DRP = DRP .* (~identical_choice); % (added 20.02.19)
+
+% number of observations
+[obs_num , ~] = size(DRP);
+
+% when we use an algorithm to find all minimal cycles, we don't want that 
+% there would be edges from a vertex to itself, so we make sure it won't occur
+for i=1:obs_num
+    DRP(i,i) = 0;
+end
+
+
+
+% find all minimal cycles (cycles that don't contain sub-cycles)
+
+% --- according to SARP ---
+% % NOTE! When using this function to calculate HM according to SARP, the
+% % given DRP must NOT include a relation between identical choices, that
+% % is: we use DRP & ~identical_choice instead of DRP
+% cycles = find_all_minimal_cycles (DRP & ~identical_choice);  
+
+% --- according to GARP ---
+cycles = find_all_minimal_GARP_cycles (DRP, SDRP);
+
+
+
+% now we make preparations for the use of integer programming (using intlinprog)   
+
+% weight of each observations
+weights = ones(1,obs_num);
+f = weights';
+
+% all values are integers that receive value of either 0 or 1
+intcon = 1:obs_num;
+lb = zeros(1, obs_num);
+ub = ones(1, obs_num);
+
+% the Ax<=b restriction will make sure that each cycle is covered by at least one observation 
+cycles_num = length(cycles);
+A = zeros(cycles_num, obs_num);  % initialization
+b = - ones(cycles_num, 1);
+for c = 1:cycles_num
+    currect_cycle = cycles{c};
+    A(c, currect_cycle) = -1;
+end
+
+% avoid displaying algorithm stages in command window
+intlinprog_options = optimoptions('intlinprog', 'Display','off');
+
+% find the raw HM Index (minimum number of observations that need to be dropped) 
+[x, fval, exitflag, ~] = intlinprog(f, intcon, A,b, [],[], lb,ub, [], intlinprog_options);
+
+if exitflag == 1
+    % assigning to the matrices
+    HM_sum = fval;
+    HM_mean = fval / obs_num;
+    solution_vector = x;
+else
+    % assigning nan values and ending the function
+    HM_sum = nan;
+    HM_mean = nan;
+    HM_raw_residuals = nan(1, obs_num);
+    return
+end
+
+
+
+
+
+% out-of-sample
+
+% initialization, and also this is what will be returned if residuals are not required   
+HM_raw_residuals = nan(1, obs_num); 
+% we need to reduce the sizes of these vectors by one
+% these vectors (except intcon) contain the same value in all cells so we can truncate them any way we want  
+intcon_residuals = intcon(1:(end-1));
+lb_residuals = lb(2:end);
+ub_residuals = ub(2:end);
+f_residuals = f(2:end);
+
+if out_of_sample_required
+    
+%     if obs_num == 1
+%         % a single observation cannot violate GARP, so...
+%         HM_raw_residuals(i) = 0;
+    
+    for i=1:obs_num
+        
+        if solution_vector(i) == 1
+            % then we know that dropping this observations will reduce by 1
+            % the number of observations needed to be dropped  
+            % (this check is not necessary, but it saves some time)  
+            HM_raw_residuals(i) = 1;
+        else
+            % we need to check if dropping this observation reduces by 1 or
+            % not reduces the number of observations needed to be dropped
+            
+            % cycles that observation i is part of them
+            cycles_that_include_observation_i = logical(A(:,i));
+            
+            % a matrix A without observation i and without all minimal
+            % cycles that include observation i
+            A_without_obs_i = A(~cycles_that_include_observation_i , [1:(i-1),(i+1):end]);
+            b_without_obs_i = b(~cycles_that_include_observation_i);
+            
+            if isempty(A_without_obs_i)
+                % then removing this observations removed all the cycles,
+                % and since we only use this function on datasets that
+                % violate GARP, we know that dropping this observation
+                % reduces the number of observations that need to be dropped (by one)  
+                HM_raw_residuals(i) = 1;
+            end
+            
+            % find the raw HM Index without this observations 
+            [~, fval, exitflag, ~] = intlinprog(f_residuals, intcon_residuals, A_without_obs_i,b_without_obs_i, [],[], lb_residuals,ub_residuals, [], intlinprog_options);
+            if exitflag == 1
+                % assigning to the raw residuals vector
+                HM_raw_residuals(i) = HM_sum - fval;   % will be either 1 or 0 
+            end
+        end
+        
+    end
+    
+end
+
+
+
+
+
+end
--- Consistency Indices/Houtman Maks/HPZ_Houtman_Maks_Manager.m
+++ Consistency Indices/Houtman Maks/HPZ_Houtman_Maks_Manager.m
@@ -1,4 +1,4 @@
-function [HM, HM_residuals, HM_raw, HM_raw_residuals, HM_Mat] = HPZ_Houtman_Maks_Manager (HOUTMAN_flags, DRP, SDRP, RP, identical_choice, num_of_goods, residuals_waitbar, current_run, total_runs, subject_ID)
+function [HM, HM_residuals, HM_raw, HM_raw_residuals, HM_Mat] = HPZ_Houtman_Maks_Manager (HOUTMAN_flags, DRP, SDRP, RP, identical_choice)
 
 % number of observations
 [obs_num , ~] = size(DRP);
@@ -11,275 +11,65 @@ function [HM, HM_residuals, HM_raw, HM_raw_residuals, HM_Mat] = HPZ_Houtman_Maks
 HM_Mat = [];
 
 
+% whether out-of-sample is required by the user
+out_of_sample_required = (HOUTMAN_flags(2) && HOUTMAN_flags(4));
 
-    
-if num_of_goods == 2
-    % if there are only 2 goods
-    
-    % thanks to the Rose (1958) Theorem, we can use an algorithm
-    % for minimum vertex cover when there are only 2 goods
-    
-    
-    
-    % a matrix that represents the graph
-    GARP_HM = DRP .* SDRP';
-    % for SARP (instead of GARP) use this line. if you change to SARP, change the SDRP to DRP in HPZ_Indices_Problem_Distribute ahead as well. 
-    % also, the DRP used for SARP must be one that does not include identical choices. 
-    %GARP_HM = DRP .* DRP';  
-    GARP_couples = max(GARP_HM, GARP_HM');
-    
-    if ~(HOUTMAN_flags(2) && HOUTMAN_flags(4))
-        % we just calculate the index and that's it
-        [HM, HM_raw, ~] = HPZ_Houtman_Maks_Two_Goods_Only (GARP_couples, 0);
-        % these initializations are just to avoid bugs/crashs
-        HM_raw_residuals = zeros(obs_num, 1);
-        HM_residuals = zeros(obs_num, 1);
+
+% divide the observations to smallest possible subsets such that
+% observations from different subsets never belong to one cycle that
+% violates the relevant-GARP
+[subsets_of_observations, num_of_subsets] = HPZ_Indices_Problem_Distribute(RP, RP);
+
+% initialization of how many observations should we drop
+HM_raw_accumulated = 0;
+% initialization of HM raw per subset
+HM_raw_per_subset = nan(num_of_subsets, 1);
+
+% initialization (it must be initialized to 0)
+% raw_residuals are 0 if deleting this observation will not improve the HM index,  
+% and are 1 if deleting this observation will not improve the HM index.
+HM_raw_residuals = nan(obs_num, 1);
+
+% now we calculate the HM separately for each subset
+for i=1:num_of_subsets
+
+    if length(subsets_of_observations{i}) > 1
+
+        %subset = subsets_of_observations{i}
+
+        % create new DRP & SDRP matrices with only the observations of this subset 
+        subset_DRP = DRP(subsets_of_observations{i} , subsets_of_observations{i});
+        subset_SDRP = SDRP(subsets_of_observations{i} , subsets_of_observations{i});
+        subset_identical_choice = identical_choice(subsets_of_observations{i} , subsets_of_observations{i});
+        
+        % calculate the HM index for this subset of choices
+        [~, subset_HM_sum, subset_HM_raw_residuals] = HPZ_Houtman_Maks_Index (subset_DRP, subset_SDRP, subset_identical_choice, out_of_sample_required);
+        
+        % update the accumulated HM
+        HM_raw_accumulated = HM_raw_accumulated + subset_HM_sum;
+        % update / assign to the subset HM
+        HM_raw_per_subset(i) = subset_HM_sum;
+
+        % update the residuals for these observations
+        % if the subset residual is bigger than the subset HM, it means
+        % that the raw residual was 0, and if it is smaller, it means that
+        % the raw residual was 1
+        HM_raw_residuals(subsets_of_observations{i}) = subset_HM_raw_residuals; 
         
     else
         
-        % divide the observations to smallest possible subsets such that
-        % observations from different subsets never belong to one 2-length cycle 
-        % (this allows the out-of-sample to work faster)
-        [subsets_of_observations, num_of_subsets] = HPZ_Indices_Problem_Distribute(DRP, SDRP);
-        
-        % initializations
-        HM_raw_accumulated = 0;
-        HM_raw_per_subset = zeros(num_of_subsets, 1);
-        HM_raw_pseudo_residuals = zeros(obs_num, 1);
-        HM_raw_residuals = zeros(obs_num, 1);
-        
-        % now we calculate the Houtman-Maks separately for each subset
-        for i=1:num_of_subsets
-            
-            if length(subsets_of_observations{i}) == 1
-                % no need to drop anything
-                HM_raw_per_subset(i) = 0;
-            else
-                % extract the relevant part of the graph
-                subset_GARP_couples = GARP_couples(subsets_of_observations{i} , subsets_of_observations{i});
-                
-                % calculate the Houtman-Maks index for this subset of choices
-                [~, subset_HM_raw, subset_HM_raw_pseudo_residuals] = HPZ_Houtman_Maks_Two_Goods_Only (subset_GARP_couples, 0);
-                HM_raw_per_subset(i) = subset_HM_raw;
-                HM_raw_pseudo_residuals(subsets_of_observations{i}) = subset_HM_raw_pseudo_residuals;
-                % update the accumulated HM
-                HM_raw_accumulated = HM_raw_accumulated + subset_HM_raw;
-            end
-        end
-        
-        % the final HM index
-        HM_raw = HM_raw_accumulated;
-        HM = HM_raw_accumulated / obs_num;
-
-        
-        
-        % define the waitbar
-        if (residuals_waitbar)
-            waitbar_name = char(strcat(HPZ_Constants.waitbar_name_estimation, {' '}, '(', HPZ_Constants.current_run_waitbar, {' '}, num2str(current_run), {' '}, HPZ_Constants.total_runs_waitbar, {' '}, num2str(total_runs),')'));
-            waitbar_msg = char(strcat(HPZ_Constants.waitbar_recovery, {' '}, num2str(subject_ID), {' '}, HPZ_Constants.waitbar_residuals_HOUTMAN));
-            new_bar_val = 0;
-            h_wb = wide_waitbar(new_bar_val, {waitbar_msg, ''}, waitbar_name, HPZ_Constants.waitbar_width_multiplier, [0,0.12]);
-        end
-        
-        % now for the residuals themselves
-        for i=1:num_of_subsets
-
-            current_subset = subsets_of_observations{i};
-            
-            if length(current_subset) == 1
-                
-                % no need to drop anything
-                HM_raw_residuals(current_subset(1)) = 0;
-                
-                % updating the waitbar
-                if (residuals_waitbar)
-                    new_bar_val = new_bar_val + 1/obs_num;
-                    waitbar(new_bar_val, h_wb, {waitbar_msg , char(strcat({'Completed '}, num2str(new_bar_val*obs_num), {' observations out of '}, num2str(obs_num)))});
-                end
-                
-            else
-                
-                % extract the relevant part of the graph
-                subset_GARP_couples = GARP_couples(current_subset , current_subset);
-                
-                for j=1:length(current_subset)
-                    
-                    if HM_raw_pseudo_residuals(current_subset(j)) == 1
-                        % we know it is 1
-                        HM_raw_residuals(current_subset(j)) = 1;
-                    else
-                        % we need to calculate it, to check if it is 0 or 1 
-                        % creating truncated matrices
-                        remaining_indices = [1:(j-1) , (j+1):length(current_subset)];
-                        truncated_GARP_couples = subset_GARP_couples(remaining_indices , remaining_indices);
-                        [~, out_sample_HM_raw, ~] = HPZ_Houtman_Maks_Two_Goods_Only (truncated_GARP_couples, 0);
-                        HM_raw_residuals(current_subset(j)) = HM_raw_per_subset(i) - out_sample_HM_raw;
-                    end
-                    
-                    % updating the waitbar
-                    if (residuals_waitbar)
-                        new_bar_val = new_bar_val + 1/obs_num;
-                        waitbar(new_bar_val, h_wb, {waitbar_msg , char(strcat({'Completed '}, num2str(new_bar_val*obs_num), {' observations out of '}, num2str(obs_num)))});
-                    end
-                    
-                end
-            end
-        end
-        
-        % finalizing the residuals
-        HM_residuals = (HM_raw - HM_raw_residuals) / (obs_num - 1);
-        
-        % close the waitbar
-        if (residuals_waitbar)
-            close(h_wb);
-        end
-        
-    end
-
-
-    
-else
-    % if there are more than 2 goods
-    
-    % when there are more than 2 goods, without the Rose (1958) Theorem, 
-    % we use an algorithm for minimum vertex cover in hypergraph 
-    
-    
-    
-    % divide the observations to smallest possible subsets such that
-    % observations from different subsets never belong to one cycle that
-    % violates the relevant-GARP
-    [subsets_of_observations, num_of_subsets] = HPZ_Indices_Problem_Distribute(RP, RP);
-    
-    % initialization of how many observations should we drop
-    HM_raw_accumulated = 0;
-    % initialization of HM raw per subset
-    HM_raw_per_subset = zeros(num_of_subsets, 1);
-    
-    % initialization (it must be initialized to 0)
-    % raw_residuals are 0 if deleting this observation will not improve the HM index,  
-    % and are 1 if deleting this observation will not improve the HM index.
-    HM_raw_pseudo_residuals = zeros(obs_num, 1);
-
-    % now we calculate the HM separately for each subset
-    for i=1:num_of_subsets
-        
-        if length(subsets_of_observations{i}) > 1
-
-            %subset = subsets_of_observations{i}
-
-            % create new DRP & SDRP matrices with only the observations of this subset 
-            subset_DRP = DRP(subsets_of_observations{i} , subsets_of_observations{i});
-            subset_SDRP = SDRP(subsets_of_observations{i} , subsets_of_observations{i});
-            subset_identical_choice = identical_choice(subsets_of_observations{i} , subsets_of_observations{i});
-            
-            % calculate the HM index for this subset of choices
-            %[~, ~, subset_HM_raw, subset_HM_raw_residuals, subset_HM_exact] = HPZ_Houtman_Maks_efficiency_index (residuals_flag, subset_DRP, subset_SDRP, is_2_goods);
-
-            % (Next is a work still in progress, that will replace the above function in future versions) 
-            %[subset_GARP, ~, ~] = GARP_based_on_DRP_and_SDRP(subset_DRP, subset_SDRP);
-            [~, subset_HM_raw, ~, subset_HM_raw_pseudo_residuals] = HPZ_Houtman_Maks_Any_Number_Of_Goods (subset_DRP, subset_SDRP, subset_identical_choice);
-            %subset_weights = ones(1,length(subsets_of_observations{i}));
-            %[~, subset_HM_raw, subset_HM_raw_pseudo_residuals, subset_HM_exact] = HPZ_Houtman_Maks_Weighted_Cycles_Approach (subset_SDRP, subset_weights);
-
-            % update the accumulated HM
-            HM_raw_accumulated = HM_raw_accumulated + subset_HM_raw;
-            % update / assign to the subset HM
-            HM_raw_per_subset(i) = subset_HM_raw;
-            
-            % update the residuals for these observations
-            % if the subset residual is bigger than the subset HM, it means
-            % that the raw residual was 0, and if it is smaller, it means that
-            % the raw residual was 1
-            HM_raw_pseudo_residuals(subsets_of_observations{i}) = subset_HM_raw_pseudo_residuals;
-
-        end
-
-    end
-
-    % the final HM index
-    HM_raw = HM_raw_accumulated;
-    HM = HM_raw_accumulated / obs_num;
-
-    % initializing the residuals (just to prevent program from crashing)
-    HM_raw_residuals = 0;
-    HM_residuals = 0;
-    
-    
-    
-    if HOUTMAN_flags(2) && HOUTMAN_flags(4)
-        
-        % define the waitbar
-        if (residuals_waitbar)
-            waitbar_name = char(strcat(HPZ_Constants.waitbar_name_estimation, {' '}, '(', HPZ_Constants.current_run_waitbar, {' '}, num2str(current_run), {' '}, HPZ_Constants.total_runs_waitbar, {' '}, num2str(total_runs),')'));
-            waitbar_msg = char(strcat(HPZ_Constants.waitbar_recovery, {' '}, num2str(subject_ID), {' '}, HPZ_Constants.waitbar_residuals_HOUTMAN));
-            new_bar_val = 0;
-            h_wb = wide_waitbar(new_bar_val, {waitbar_msg, ''}, waitbar_name, HPZ_Constants.waitbar_width_multiplier, [0,0.12]);
-        end
-        
-        % initialization
-        HM_raw_residuals = zeros(obs_num, 1);
-        
-        % now for the residuals themselves
-        for i=1:num_of_subsets
-
-            current_subset = subsets_of_observations{i};
-            
-            if length(current_subset) == 1
-                
-                % no need to drop anything
-                HM_raw_residuals(current_subset(1)) = 0;
-                
-                % updating the waitbar
-                if (residuals_waitbar)
-                    new_bar_val = new_bar_val + 1/obs_num;
-                    waitbar(new_bar_val, h_wb, {waitbar_msg , char(strcat({'Completed '}, num2str(new_bar_val*obs_num), {' observations out of '}, num2str(obs_num)))});
-                end
-                
-            else
-                % create new DRP & SDRP matrices with only the observations of this subset 
-                subset_DRP = DRP(subsets_of_observations{i} , subsets_of_observations{i});
-                subset_SDRP = SDRP(subsets_of_observations{i} , subsets_of_observations{i});
-                subset_identical_choice = identical_choice(subsets_of_observations{i} , subsets_of_observations{i});
-                
-                for j=1:length(current_subset)
-                    
-                    if HM_raw_pseudo_residuals(current_subset(j)) == 1
-                        % we know it is 1
-                        HM_raw_residuals(current_subset(j)) = 1;
-                    else
-                        % we need to calculate it, to check if it is 0 or 1 
-                        % creating truncated matrices
-                        remaining_indices = [1:(j-1) , (j+1):length(current_subset)];
-                        truncated_DRP = subset_DRP(remaining_indices , remaining_indices);
-                        truncated_SDRP = subset_SDRP(remaining_indices , remaining_indices);
-                        truncated_identical_choice = subset_identical_choice(remaining_indices , remaining_indices);
-                        [~, out_sample_HM_raw, ~, ~] = HPZ_Houtman_Maks_Any_Number_Of_Goods (truncated_DRP, truncated_SDRP, truncated_identical_choice);
-                        HM_raw_residuals(current_subset(j)) = HM_raw_per_subset(i) - out_sample_HM_raw;
-                    end
-                    
-                    % updating the waitbar
-                    if (residuals_waitbar)
-                        new_bar_val = new_bar_val + 1/obs_num;
-                        waitbar(new_bar_val, h_wb, {waitbar_msg , char(strcat({'Completed '}, num2str(new_bar_val*obs_num), {' observations out of '}, num2str(obs_num)))});
-                    end
-                end
-            end
-        end
-        
-        % finalizing the residuals
-        HM_residuals = (HM_raw - HM_raw_residuals) / (obs_num - 1);
-
-        
-        % close the waitbar
-        if (residuals_waitbar)
-            close(h_wb);
-        end
-        
+        % we need to assign a raw residual of 0 to this single observation  
+        HM_raw_residuals(subsets_of_observations{i}) = 0;
         
     end
 
 end
+
+% the final HM index
+HM_raw = HM_raw_accumulated;
+HM = HM_raw_accumulated / obs_num;
+% finalizing the residuals
+HM_residuals = (HM_raw - HM_raw_residuals) / (obs_num - 1);
 
 
 
