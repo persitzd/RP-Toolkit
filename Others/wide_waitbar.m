6fcb4a99ab2ca9c3d615e54c42ec0cd3e88ed6c4
Oriel Nofekh <42373011+Orielno@users.noreply.github.com>
2020-01-31 09:12:51 +0200

    Adapting to new Matlab version (R2019b)
    Starting version R2019b Matlab does not allow to change the size of the GUI after it is initialized, so this function causes an error and a crash. I "fixed" it by adding an "if-else" that checks the version, and if it is 2019 or later it doesn't attempts to make the waitbar wider.

--- Others/wide_waitbar.m
+++ Others/wide_waitbar.m
@@ -13,12 +13,16 @@ end
 % Create the waitbar
 wb = waitbar(bar_value, message, 'name', name);
 
-% Change the Units Poreperty of the figure and all the children
-set(findall(wb),'Units', 'normalized');
-% Change the size of the figure
-wb_position = get(wb, 'Position');
-set(wb,'Position', [wb_position(1)-wb_position(3)*(width_multiplier-1)/2+shifts(1) , wb_position(2)-shifts(2) , wb_position(3)*width_multiplier , wb_position(4)]);
-
+if str2double(strip(strip(version('-release'), 'a'), 'b')) >= 2019
+    % do nothing; starting 2019b it is not possible to change the size of
+    % the waitbar after it is created (causes an error and a crash)
+else
+    % Change the Units Poreperty of the figure and all the children
+    set(findall(wb),'Units', 'normalized');
+    % Change the size of the figure
+    wb_position = get(wb, 'Position');
+    set(wb,'Position', [wb_position(1)-wb_position(3)*(width_multiplier-1)/2+shifts(1) , wb_position(2)-shifts(2) , wb_position(3)*width_multiplier , wb_position(4)]);
+end
 
 % this command is needed in order to refresh the waitbar graphics immediately 
 waitbar(bar_value, wb);
