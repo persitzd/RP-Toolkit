0131254d0fabf9c4ca75f2563fcb3cc2f7b96c6c
Oriel Nofekh <42373011+Orielno@users.noreply.github.com>
2021-01-29 12:53:09 +0200

    Print Data of Single Subject Option

--- HPZ_Interface.m
+++ HPZ_Interface.m
@@ -75,6 +75,7 @@ BI_threshold = cell(1,1);
 max_starting_points = cell(1,1);
 one_residuals_file = cell(1,1);
 debugger_mode = cell(1,1);
+print_single_subject = cell(1,1);
 waitbar_settings = cell(1,1);
 Varian_algorithm_settings = cell(1,1);
 print_precision = cell(1,1);
@@ -94,7 +95,7 @@ while (another_run)
     % (2) In the last screen, the user is asked whether he desires to make
     %     more runs (with a different data set / method / etc.), or whther
     %     he is finished and the program should start running (another_run==false) 
-    [ok, data_matrix{i}, subjects_index{i}, action_flag{i}, Graph_flags{i}, power_test_settings{i}, GARP_flags{i}, AFRIAT_flags{i}, VARIAN_flags{i}, HOUTMAN_flags{i}, MPI_flags{i}, pref_class{i}, choice_set_type{i}, function_flag{i}, numeric_flag{i}, param1_restrictions{i}, param2_restrictions{i}, fix_corners{i}, metric_flag{i}, aggregation_flag{i}, max_time_estimation{i}, min_counter{i}, parallel_flag{i}, output_file_config{i}, write_all_flag{i}, bootstrap_flag{i}, file_val_str{i}, residual_flag{i}, in_sample_flag{i}, out_sample_flag{i}, bootstrap_sample_sizes{i}, bootstrap_significance_level{i}, BI_threshold{i}, max_starting_points{i}, one_residuals_file{i}, debugger_mode{i}, waitbar_settings{i}, Varian_algorithm_settings{i}] = HPZ_All_Screens_Manager(main_folder, runs_counter);
+    [ok, data_matrix{i}, subjects_index{i}, action_flag{i}, Graph_flags{i}, power_test_settings{i}, GARP_flags{i}, AFRIAT_flags{i}, VARIAN_flags{i}, HOUTMAN_flags{i}, MPI_flags{i}, pref_class{i}, choice_set_type{i}, function_flag{i}, numeric_flag{i}, param1_restrictions{i}, param2_restrictions{i}, fix_corners{i}, metric_flag{i}, aggregation_flag{i}, max_time_estimation{i}, min_counter{i}, parallel_flag{i}, output_file_config{i}, write_all_flag{i}, bootstrap_flag{i}, file_val_str{i}, residual_flag{i}, in_sample_flag{i}, out_sample_flag{i}, bootstrap_sample_sizes{i}, bootstrap_significance_level{i}, BI_threshold{i}, max_starting_points{i}, one_residuals_file{i}, debugger_mode{i}, print_single_subject{i}, waitbar_settings{i}, Varian_algorithm_settings{i}] = HPZ_All_Screens_Manager(main_folder, runs_counter);
     % if the user pressed "cancel" in one of the screens, then this run was cancelled 
     if (ok == 0)
         runs_counter = runs_counter - 1;
@@ -126,11 +127,11 @@ for i = 1:runs_counter
         % All calculations and printing to results file occur here
         if action_flag{i} == HPZ_Constants.Consistency_action
             % Consistency Indices
-            HPZ_Consistency_Indices_Manager(data_matrix{i}, subjects_index{i}, choice_set_type{i}, Graph_flags{i}, power_test_settings{i}, GARP_flags{i}, AFRIAT_flags{i}, VARIAN_flags{i}, HOUTMAN_flags{i}, MPI_flags{i}, max_time_estimation{i}, print_precision{i}, one_residuals_file{i}, waitbar_settings{i}, Varian_algorithm_settings{i}, main_folder_for_results, i, runs_counter);  
+            HPZ_Consistency_Indices_Manager(data_matrix{i}, subjects_index{i}, choice_set_type{i}, Graph_flags{i}, power_test_settings{i}, GARP_flags{i}, AFRIAT_flags{i}, VARIAN_flags{i}, HOUTMAN_flags{i}, MPI_flags{i}, max_time_estimation{i}, print_precision{i}, one_residuals_file{i}, print_single_subject{i}, waitbar_settings{i}, Varian_algorithm_settings{i}, main_folder_for_results, i, runs_counter);  
 
         elseif (action_flag{i} == HPZ_Constants.NLLS_action) || (action_flag{i} == HPZ_Constants.MMI_action) || (action_flag{i} == HPZ_Constants.BI_action)
             % Parameters Estimation (NLLS / MMI / BI)
-            HPZ_Estimation_Manager(data_matrix{i}, subjects_index{i}, choice_set_type{i}, action_flag{i}, pref_class{i}, function_flag{i}, numeric_flag{i}, param1_restrictions{i}, param2_restrictions{i}, fix_corners{i}, metric_flag{i}, aggregation_flag{i}, max_time_estimation{i}, min_counter{i}, parallel_flag{i}, output_file_config{i}, write_all_flag{i}, bootstrap_flag{i}, file_val_str{i}, residual_flag{i}, in_sample_flag{i}, out_sample_flag{i}, print_precision{i}, bootstrap_sample_sizes{i}, bootstrap_significance_level{i}, BI_threshold{i}, max_starting_points{i}, one_residuals_file{i}, debugger_mode{i}, waitbar_settings{i}, main_folder_for_results, i, runs_counter);
+            HPZ_Estimation_Manager(data_matrix{i}, subjects_index{i}, choice_set_type{i}, action_flag{i}, pref_class{i}, function_flag{i}, numeric_flag{i}, param1_restrictions{i}, param2_restrictions{i}, fix_corners{i}, metric_flag{i}, aggregation_flag{i}, max_time_estimation{i}, min_counter{i}, parallel_flag{i}, output_file_config{i}, write_all_flag{i}, bootstrap_flag{i}, file_val_str{i}, residual_flag{i}, in_sample_flag{i}, out_sample_flag{i}, print_precision{i}, bootstrap_sample_sizes{i}, bootstrap_significance_level{i}, BI_threshold{i}, max_starting_points{i}, one_residuals_file{i}, debugger_mode{i}, print_single_subject{i}, waitbar_settings{i}, main_folder_for_results, i, runs_counter);
 
         end
     catch error_var
